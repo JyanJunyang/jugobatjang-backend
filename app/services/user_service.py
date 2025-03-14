810a0ea5e75fd4843a7a135e0a84905810a8d933
chilln <kimjihan77@naver.com>
2025-03-14 15:27:43 +0900

    feat:신규유저 생성 메소드

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -26,3 +26,23 @@ class UserService:
         if user_id is None:
             raise NotFoundError(detail="해당 사용자를 찾을 수 없습니다.")
         return user_id
+
+    async def signup_new_user(self, user_data):
+        """신규유저 생성 메소드."""
+        try:
+            user = Users(**user_data)
+            self.db.add(user)
+            self.db.flush()
+            self.db.refresh(user)
+            self.db.commit()
+            return user.id
+        except IntegrityError as e:
+            self.db.rollback()
+            error_msg = str(e).lower()
+            if "duplicate entry" in error_msg and "email" in error_msg:
+                raise DuplicatedErrorException(
+                    detail="이미 사용 중인 이메일 주소입니다."
+                ) from e
+        except Exception as e:
+            self.db.rollback()
+            raise UnknownErrorException(detail=str(e)) from e
