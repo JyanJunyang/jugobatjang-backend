54e2f3c284379325d7c467a2cd1f4e400675c863
chilln <kimjihan77@naver.com>
2025-03-14 15:27:24 +0900

    feat:social_id값으로 user_id값 반환하는 메소드

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -19,3 +19,10 @@ class UserService:
         if user is None:
             return False
         return True
+
+    async def get_user_id(self, social_id: str):
+        """social_id값으로 user_id값 반환하는 메소드."""
+        user_id = self.db.query(Users.id).filter(Users.social_id == social_id).scalar()
+        if user_id is None:
+            raise NotFoundError(detail="해당 사용자를 찾을 수 없습니다.")
+        return user_id
