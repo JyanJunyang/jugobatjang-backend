873c4cfa34458a775a5a7dc7549e22504caeca68
chilln <kimjihan77@naver.com>
2025-03-14 15:23:13 +0900

    feat:회원가입 및 로그인 DTO 모델 정의 - AuthDTOModel

--- app/schema/auth_schema.py
+++ app/schema/auth_schema.py
@@ -5,6 +5,20 @@ from pydantic import BaseModel, Field
 from app.model.users import LoginTypeEnum
 
 
+class AuthDTOModel(BaseModel):
+    """회원가입, 로그인 DTO"""
+
+    login_type: str = Field(..., description="로그인 유형 (KAKAO, EMAIL)")
+    social_id: str | None = Field(None, description="소셜로그인 시 id값")
+    email: str | None = Field(
+        None,
+        description="이메일 주소 (이메일 로그인일 경우 필수)",
+    )
+    password: str | None = Field(
+        None, description="비밀번호 (이메일 로그인일 경우 필수)"
+    )
+
+
 class CommonHeader(BaseModel):
     token_type: str | None = Field(None, description="토큰 타입")
     access_token: str | None = Field(None, description="액세스 토큰")
@@ -15,19 +29,7 @@ class CommonHeader(BaseModel):
     )
 
 
-class SignUpDtoModel(BaseModel):
-    """회원가입 DTO"""
-
-    login_type: LoginTypeEnum = Field(..., description="로그인 유형 (KAKAO, EMAIL)")
-    email: str | None = Field(
-        None, description="이메일 주소 (이메일 로그인일 경우 필수)"
-    )
-    password: str | None = Field(
-        None, description="비밀번호 (이메일 로그인일 경우 필수)"
-    )
-
-
-class SocialUserDtoModel(BaseModel):
+class SocialUserDTOModel(BaseModel):
     """소셜 로그인으로 가져오는 회원 정보"""
 
     social_id: str = Field(..., description="소셜 id")
