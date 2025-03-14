bec6fccf9b09245cff53616892d2a5e896898ba7
chilln <kimjihan77@naver.com>
2025-03-14 15:31:12 +0900

    feat:token decoding 후 user_id값 반환

--- app/core/security.py
+++ app/core/security.py
@@ -32,3 +32,30 @@ def create_jwt_refresh_token(data):
     refresh_token = jwt.encode(to_encode, REFRESH_SECRET_KEY, algorithm=ALGORITHM)
 
     return dict(refresh_token=refresh_token, refresh_expires_in=exp)
+
+
+def decode_jwt_payload(access_token: str, refresh_token: str):
+    """token decoding 후 user_id값 반환"""
+    try:
+        if not access_token or not refresh_token:
+            raise RequestDataMissingException(detail="토큰이 필요합니다.")
+        # access_token 디코딩
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
+        user_id = int(payload.get("sub"))
+        return dict(user_id=user_id)
+    except ExpiredSignatureError:
+        try:
+            # access_token 만료 시 refresh_token으로 token 갱신 및 user_id값 반환
+            payload = jwt.decode(
+                refresh_token, REFRESH_SECRET_KEY, algorithms=ALGORITHM
+            )
+            user_id = payload.get("sub")
+            data = {
+                "sub": user_id,
+            }
+            access_token = create_jwt_access_token(data=data)
+            refresh_token = create_jwt_refresh_token(data=data)
+            return dict(user_id=user_id, token={**access_token, **refresh_token})
+        except ExpiredSignatureError:
+            # refresh_token도 만료됐을 경우 raise exception
+            raise TokenExpiredException() from None
