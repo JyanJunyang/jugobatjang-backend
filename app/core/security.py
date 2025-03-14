c498be2699b5a0d9778177a1dc3eda7056a8c0d0
chilln <kimjihan77@naver.com>
2025-03-14 15:30:48 +0900

    feat:refresh_token 생성 메소드

--- app/core/security.py
+++ app/core/security.py
@@ -21,3 +21,14 @@ def create_jwt_access_token(data):
     access_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return dict(access_token=access_token, expires_in=exp)
+
+
+def create_jwt_refresh_token(data):
+    """refresh_token 생성 메소드."""
+    to_encode = data.copy()
+    exp = int(datetime.now().timestamp()) + 259200  # 3일
+    to_encode.update({"exp": exp})
+
+    refresh_token = jwt.encode(to_encode, REFRESH_SECRET_KEY, algorithm=ALGORITHM)
+
+    return dict(refresh_token=refresh_token, refresh_expires_in=exp)
