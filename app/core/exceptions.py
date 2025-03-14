ace9a42a095978e4f3119aef8c93db109ff336df
chilln <kimjihan77@naver.com>
2025-03-14 15:28:46 +0900

    feat:NotFoundError Exception

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -20,3 +20,12 @@ class UnknownErrorException(HTTPException):
         self, detail: Optional[str] = None, headers: Optional[Dict[str, Any]] = None
     ):
         super().__init__(status.HTTP_500_INTERNAL_SERVER_ERROR, detail, headers)
+
+
+class NotFoundError(HTTPException):
+    """찾을 수 없는 데이터 오류"""
+
+    def __init__(
+        self, detail: Optional[str] = None, headers: Optional[Dict[str, Any]] = None
+    ):
+        super().__init__(status.HTTP_404_NOT_FOUND, detail, headers)
