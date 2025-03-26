88c3ea27fc985674d1fb9c104aa430428e3b8770
chilln <kimjihan77@naver.com>
2025-03-26 11:51:16 +0900

    fix:기록 상세조회 Response 필드 수정

--- app/schema/records.py
+++ app/schema/records.py
@@ -35,14 +35,14 @@ class RecordSearchResponseDTOModel(BaseModel):
 
 class RecordDetailDTOModel(BaseModel):
     id: int = Field(..., description="기록 ID")
-    name: str = Field(..., description="기록 이름")
     amount: int = Field(..., description="금액")
     is_received: int = Field(..., description="받은내역 : 1, 준 내역 : 0")
     phone: str | None = Field(default=None, description="대상자의 핸드폰 번호")
     status: str | None = Field(default=None, description="참여 여부 ")
     memo: str | None = Field(default=None, description="메모")
-    event_date: datetime = Field(..., description="경조사 일정")
-    created_at: datetime = Field(..., description="생성날짜")
+    date: datetime = Field(..., description="금액을 주거나 받은 날짜")
+    peer_name: str = Field(..., description="금액을 주거나 받은 상대방의 이름")
+    calendar_date: datetime = Field(..., description="경조사 일정")
     event_type_id: int = Field(..., description="경조사 ID")
     event_type_name: str = Field(..., description="경조사 종류 이름")
     event_type_color_code: str = Field(..., description="경조사 커스텀 색상")
